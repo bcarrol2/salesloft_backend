37d4df1270dd5903bb5b9e3c80e1cab38eec0512
Blair <blairacarroll@gmail.com>
2020-03-26 03:33:31 -0400

    count email character method done

--- salesloft_app/app/controllers/api/v1/users_controller.rb
+++ salesloft_app/app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,25 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def user_email_characters
-        email_characters = @user.email.split('@')[0] 
+        email_characters = @user.email.split('@')[0]
+        render json: email_characters
+    end
+
+    def count_letters(user_email_characters)
+        counted = {}
+
+        chars = user_email_characters.split("")
+        for char in chars
+            if char == " "
+            next
+            elsif counted.has_key? char
+            counted[char] = counted[char] + 1   
+            else
+            counted[char] = 1
+            end
+        end
+
+        render json: counted
     end
 
 end
