60f6f761892e63d89d752a27bd3b7c3a17e90c9a
Blair <blairacarroll@gmail.com>
2020-03-26 12:36:23 -0400

    compare close enough emails method added

--- salesloft_app/app/controllers/api/v1/users_controller.rb
+++ salesloft_app/app/controllers/api/v1/users_controller.rb
@@ -32,4 +32,19 @@ class Api::V1::UsersController < ApplicationController
         counted
     end
 
+    def email_difference_percent(email, comparing_emails)
+        close_enough = []
+
+        comparing_emails.each { |compared_email|
+            return if email === compared_email
+            longer_email = [email.size, compared_email.size].max
+            exact = email.each_char.zip(compared_email.each_char).select { |a,b| a == b }.size
+            percent = (longer_email - exact) / email.size.to_f
+            if percent < 0.6
+                close_enough << compared_email unless percent == 0.0
+            end
+        }
+        close_enough
+    end
+
 end
